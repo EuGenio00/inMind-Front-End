c0aaa126f169072de0345d1aa48f6c1ca87cde75
EuGenio00 <eugeniotchanjongombe10@gmail.com>
2023-10-11 18:49:46 -0300

    trabalhando nas rotas base do proj

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,22 @@
 import "./App.css";
-import {BrowserRouter, Routes} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Components/Paginas/Home";
+import Login from "./Components/Paginas/Login";
+import Cadastro from "./Components/Paginas/Cadastro";
 
 function AppRoutes() {
   return (
-  <BrowserRouter>
-  <Routes>
-    
-  </Routes>
-  </BrowserRouter>);
+    <BrowserRouter>
+      <Routes>
+        <Route>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/cadastro" element={<Cadastro />} />
+        </Route>
+        <Route path="*" element={<div>Página não encontrada </div>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default AppRoutes;
